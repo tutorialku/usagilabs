423807ca97031068b731f9eb57cf070e3411f806
Tutorialku <tutorialku1@gmail.com>
2016-09-14 15:43:39 +0800

    Add files via upload

---
+++ fb_us.js
@@ -0,0 +1,2 @@
+// Facebook ID
+!function(e,t,n){var o,c=e.getElementsByTagName(t)[0];e.getElementById(n)||(o=e.createElement(t),o.id=n,o.src="//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
