07954352fc6a95389442212d6e350177ae77eb0e
Tutorialku <tutorialku1@gmail.com>
2016-10-23 03:37:26 +0800

    Update fb_id.js

--- fb_id.js
+++ fb_id.js
@@ -1,2 +1,2 @@
 // Facebook ID
-!function(e,t,n){var o,c=e.getElementsByTagName(t)[0];e.getElementById(n)||(o=e.createElement(t),o.id=n,o.src="//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.7",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
+!function(e,n,t){var c,o=e.getElementsByTagName(n)[0];e.getElementById(t)||(c=e.createElement(n),c.id=t,c.async=!0,c.src="//connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v2.8",o.parentNode.insertBefore(c,o))}(document,"script","facebook-jssdk");
